b35b011843e04669b84fd30a31a9e17505f57548
rddtw099999 <rddtw099999@gmail.com>
2017-09-23 00:11:12 +0800

    GUI added

--- LeapMotionDriverWindow/GraphicUI.cs
+++ LeapMotionDriverWindow/GraphicUI.cs
@@ -11,6 +11,12 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
 
 namespace LeapMotionDriverWindow
 {
